4c072eaf1c759ad048a7717616e2f863911eaf4f
Sana Zahlan <sanazahlan@yahoo.com>
2022-04-24 03:35:10 -0700

    Prompt user to enter a number if they submit an empty form.

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,25 @@
 function beepBoop(userInput) {
   const orderedNumbers = [];
   let output;
-  if (userInput <= 0) {
-    output = "This application is for numbers that are greater than 0 only! Please enter a different number."
+  if (userInput = " ") {
+    output = "Please enter a number!"
+  } else if (userInput <= 0) {
+      output = "This application is for numbers that are greater than 0 only! Please enter a different number."
   } else {
-    for (i = 0; i <= userInput; i++) {
-      numberString = i.toString();
-      if (numberString.includes("3")) {
-        orderedNumbers.push("'Won't you be my Neighbor?'");
-      } else if (numberString.includes("2")) {
-          orderedNumbers.push("'Boop!'");
-      } else if (numberString.includes("1")) {
-          orderedNumbers.push("'Beep!'");
-      } else {
-          orderedNumbers.push(i);
+      for (i = 0; i <= userInput; i++) {
+        numberString = i.toString();
+        if (numberString.includes("3")) {
+          orderedNumbers.push("'Won't you be my Neighbor?'");
+        } else if (numberString.includes("2")) {
+            orderedNumbers.push("'Boop!'");
+        } else if (numberString.includes("1")) {
+            orderedNumbers.push("'Beep!'");
+        } else {
+            orderedNumbers.push(i);
+        }
       }
+      output = orderedNumbers.join(", ");
     }
-    output = orderedNumbers.join(", ");
-  }
   return output;
 }
 
