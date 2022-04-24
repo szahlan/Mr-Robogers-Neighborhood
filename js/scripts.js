aae3f19be835958e9c7d1f070c9510a373428f5a
Sana Zahlan <sanazahlan@yahoo.com>
2022-04-24 01:48:26 -0700

    Store output in a variable to prepare for adding user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 function beepBoop(userInput) {
   const orderedNumbers = [];
+  let output;
   if (userInput <= 0) {
-    return "This application is only for numbers that are greater than 0! Please enter a different number."
-  }
-  for (i = 0; i <= userInput; i++) {
-    numberString = i.toString();
-    if (numberString.includes("3")) {
-      orderedNumbers.push("Won't you be my Neighbor?");
-    } else if (numberString.includes("2")) {
-        orderedNumbers.push("Boop!");
-    } else if (numberString.includes("1")) {
-        orderedNumbers.push("Beep!");
-    } else {
-        orderedNumbers.push(i);
+    output = "This application is only for numbers that are greater than 0! Please enter a different number."
+  } else {
+    for (i = 0; i <= userInput; i++) {
+      numberString = i.toString();
+      if (numberString.includes("3")) {
+        orderedNumbers.push("Won't you be my Neighbor?");
+      } else if (numberString.includes("2")) {
+          orderedNumbers.push("Boop!");
+      } else if (numberString.includes("1")) {
+          orderedNumbers.push("Beep!");
+      } else {
+          orderedNumbers.push(i);
+      }
     }
+    output = orderedNumbers.join(", ");
   }
-  const output = orderedNumbers.join(", ");
   return output;
 }
