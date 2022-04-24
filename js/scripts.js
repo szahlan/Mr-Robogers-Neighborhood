8d81b9bd614ba9bbf594cb3305d6792a17aafb89
Sana Zahlan <sanazahlan@yahoo.com>
2022-04-23 23:42:11 -0700

    Return true for numbers that contain 1, 2, or 3.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,12 @@ function beepBoop(userInput) {
   const userInputNumber = parseInt(userInput);
   const orderedNumbers = [];
   for (i = 0; i <= userInputNumber; i++) {
-    orderedNumbers.push(i);
+    iString = i.toString();
+    if (iString.includes("1") || iString.includes("2") || iString.includes("3")) {
+      orderedNumbers.push(true);
+    } else {
+        orderedNumbers.push(i);
+    }
   }
   return orderedNumbers;
 }
